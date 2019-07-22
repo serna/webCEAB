75776cad56aba438b56e826c364a2a79095c7697
cesar <serna@fisica.ugto.mx>
2019-07-22 12:18:02 -0500

    La tarea periodica de actualizar los pagos atrasados de los alumnos funciona correctamente, solo actualiza la informacion de aquellos alumnos activos que su fecha de pago se la del dia actual

--- contabilidad/management/commands/actualiza_pagos_atrasados.py
+++ contabilidad/management/commands/actualiza_pagos_atrasados.py
@@ -5,14 +5,16 @@ class Command(BaseCommand):
 	help = "Actualiza el campo de pagos atrasados para todos los alumnos activos"
 	def handle(self,*args,**options):
 		inicio = datetime.date.today()
+		qs = Tarjeton.objects.filter(alumno__activo=True,proxima_fecha_de_pago=inicio)
+		for tarjeton in qs:
+			tarjeton.save()
+
 		qs = Tarjeton.objects.get(alumno=1)
 		print("Alumno",qs,qs.inicio)
 		qs.inicio = inicio
 		qs.descripcion = time.asctime()
 		qs.save()
 		print("Alumno",qs,qs.inicio,qs.descripcion)
-		#for tarjeton in qs:
-		#	tarjeton.save()
 		#ff = open("log.txt","w")
 		#ff.write("Se actulizo los registros de pagos atrasados en la fecha: %s"%str(inicio)+str(datetime.time)
 		#ff.close()
